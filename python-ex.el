1eb6276dcef7b41573456aae3ed8ffcc669a462d
podhmo <ababjam61@gmail.com>
2010-11-23 13:52:22 +0900

    bugfix: external-eval-asyc is not ansychronized process. (but initernal-async is buggy, yet)

--- python-ex.el
+++ python-ex.el
@@ -1,4 +1,10 @@
-(provide 'python-ex)
+(setq python-ex:debug-info-p t)
+
+(require 'cl)
+(require 'anything nil t)
+(defmacro python-ex:with-anything (&rest actions)
+  `(when (fboundp 'anything)
+     ,@actions))
 ;;(util-macro-install "python-ex:")
 ;; (while (re-search-forward "python-ex:" nil t 1)(compose-region (match-beginning 0) (match-end 0) ?＠))
 
@@ -21,22 +27,22 @@
          (args* (mapcar (lambda (x)
                           (cond ((and (listp x) (not (eq 'quote (car x))))
                                  (python-ex:rlet1
-                                            g
-                                            (gensym)
-                                            (push (\` ((\, g) (\, x)))
-                                                  pre-eval-sexps)))
+                                     g
+                                     (gensym)
+                                   (push (\` ((\, g) (\, x)))
+                                         pre-eval-sexps)))
                                 ((eq '<> x)
                                  (python-ex:rlet1
-                                            x*
-                                            (gensym)
-                                            (push x* partial-args)))
+                                     x*
+                                     (gensym)
+                                   (push x* partial-args)))
                                 ((eq '<\.\.\.> x)
                                  (python-ex:rlet1
-                                            x*
-                                            (gensym)
-                                            (setq dotted-p t)
-                                            (push '&rest partial-args)
-                                            (push x* partial-args)))
+                                     x*
+                                     (gensym)
+                                   (setq dotted-p t)
+                                   (push '&rest partial-args)
+                                   (push x* partial-args)))
                                 (t x)))
                         args)))
     (setq partial-args (nreverse partial-args))
@@ -53,16 +59,16 @@
          (args* (mapcar (lambda (x)
                           (cond ((eq '<> x)
                                  (python-ex:rlet1
-                                            x*
-                                            (gensym)
-                                            (push x* partial-args)))
+                                     x*
+                                     (gensym)
+                                   (push x* partial-args)))
                                 ((eq '<\.\.\.> x)
                                  (python-ex:rlet1
-                                            x*
-                                            (gensym)
-                                            (setq dotted-p t)
-                                            (push '&rest partial-args)
-                                            (push x* partial-args)))
+                                     x*
+                                     (gensym)
+                                   (setq dotted-p t)
+                                   (push '&rest partial-args)
+                                   (push x* partial-args)))
                                 (t x)))
                         args)))
     (setq partial-args (nreverse partial-args))
@@ -92,9 +98,9 @@
                      (t (error "and-let*: invalid head %s" head))))))
           bindings
           :from-end
-           t
-           :initial-value
-            (\` (progn (\,@ body)))))
+          t
+          :initial-value
+          (\` (progn (\,@ body)))))
 (put 'python-ex:and-let* 'lisp-indent-function 1)
 (defmacro python-ex:alambda
   (args &rest body)
@@ -109,8 +115,8 @@
         ((null (cdr args))
          (car args))
         (t (\` (python-ex:aif
-                          (\, (car args))
-                          (python-ex:aand (\,@ (cdr args))))))))
+                   (\, (car args))
+                   (python-ex:aand (\,@ (cdr args))))))))
 
 (defmacro python-ex:aif
   (test-form then-form &rest else-forms)
@@ -128,7 +134,7 @@
 (defvar python-ex:auto-scroll-p t)
 
 ;;; debug
-(setq python-ex:debug-info-p )
+(defvar python-ex:debug-info-p nil)
 (defmacro python-ex:debug-info (fmt &rest args)
   (if python-ex:debug-info-p 
       (python-ex:with-gensyms (str)
@@ -154,11 +160,11 @@
     (python-ex:let1 process (python-ex:proc)
       (set-process-filter process nil)
       (delete-process process))))
-    
+
 (defun python-ex:run-repl (&optional noshow cmd)
   (interactive (if current-prefix-arg
-		   (list (read-string "Run Python: " python-ex:repl-command) nil t)
-		 (list python-ex:repl-command)))
+                   (list (read-string "Run Python: " python-ex:repl-command) nil t)
+                   (list python-ex:repl-command)))
   (unless cmd (setq cmd python-ex:repl-command))
   ;;  (python-ex:check-version cmd) not implemented
   (setq python-ex:repl-command cmd) ;; danger
@@ -214,34 +220,50 @@
            (progn ,@actions)
          (setq comint-preoutput-filter-functions ,orig)))))
 
-(defmacro python-ex:with-preoutput-filter (filter &rest actions)
-  (declare (indent 1))
-  `(python-ex:with-preoutput-filters (list ,filter)
-     ,@actions))
+;; (defmacro python-ex:with-preoutput-filter (filter &rest actions)
+;;   (declare (indent 1))
+;;   `(python-ex:with-preoutput-filters (list ,filter)
+;;      ,@actions))
 
-(defun python-ex:eval-internal (source-code)
+(defun python-ex:eval-internal (source-code &optional asyncp last-call-back) ;; must not use asyncp and last-call-back directly
   (lexical-let* ((buf (list))
-                 (source-code source-code)
                  (done-p nil)
                  (output-interception
                   (lambda (str) 
                     (python-ex:debug-info  "world==== %S" str)
-                    (cond ((not python-ex:eval-reading-p)
+                    (cond ((null python-ex:eval-reading-p)
                            (python-ex:let1 str (replace-regexp-in-string "\n?>>> " "" str)
                              (push str buf))
                            (setq buf (nreverse buf) done-p t))
                           (t (push str buf)))
                     "")))
-    (python-ex:eval-internal-1 
-     :send-action (lambda () (comint-simple-send (python-ex:proc) source-code))
-     :filters (list output-interception))
-    (while (not done-p)
-      (python-ex:debug-info "outer-sleep")
-      (sleep-for 10))
-    (mapconcat 'identity buf "")))
+    (python-ex:with-lexical-bindings (last-call-back source-code asyncp)
+      (python-ex:eval-internal-1 
+       :send-action (lambda () (comint-simple-send (python-ex:proc) source-code))
+       :filters (list output-interception)
+       :async asyncp
+       :call-back 
+       (lambda ()
+         (cond (asyncp
+                (python-ex:with-lexical-bindings (last-call-back asyncp done-p buf)
+                (python-ex:wait-for 0.01 
+                                    (lambda ()
+                                      (python-ex:debug-info "python-ex:eval-internal --  async outer-sleep")
+                                      (null done-p))
+                                    (lambda ()
+                                      (python-ex:let1 r (mapconcat 'identity buf "")
+                                        (if last-call-back (funcall last-call-back r) r))))))
+               (t 
+                (while (null done-p)
+                  (python-ex:debug-info "python-ex:eval-internal --  outer-sleep")
+                  (sleep-for 0 10))
+                (python-ex:let1 r (mapconcat 'identity buf "")
+                  (if last-call-back (funcall last-call-back r) r)))))))))
 
-(defvar pythin-ex:eval-reading-p nil) ;;internal-variable for polling
-(defun* python-ex:eval-internal-1 (&key send-action filters call-back)
+
+
+(defvar python-ex:eval-reading-p nil) ;;internal-variable for polling
+(defun* python-ex:eval-internal-1 (&key send-action filters call-back async)
   (lexical-let ((prompt-rx (format "\n?%s" ">>> ")))
     (let* ((end-check-filter
             (lambda (str)
@@ -250,14 +272,21 @@
                 (setq python-ex:eval-reading-p nil))
               str))
            (filters* (cons end-check-filter filters)))
-      (setq python-ex:eval-reading-p t)
-      (funcall send-action)
       (python-ex:with-preoutput-filters filters*
-        (while python-ex:eval-reading-p ;;polling
-          (python-ex:debug-info "inner-sleep")
-          (sleep-for 0 100))
-        (when call-back (funcall call-back))))))
-;; (setq python-ex:eval-reading-p nil)
+        (setq python-ex:eval-reading-p t)
+        (funcall send-action)
+        (cond (async
+               (python-ex:debug-info "python-ex:eval-internal-1 -- async start")
+               (python-ex:wait-for 1 ;;
+                                   (lambda () 
+                                     (python-ex:debug-info "python-ex:eval-internal-1 -- reading? %s"
+                                                           python-ex:eval-reading-p)
+                                     (null python-ex:eval-reading-p))
+                                   call-back))
+              (t (while python-ex:eval-reading-p ;;polling
+                   (python-ex:debug-info "python-ex:eval-internal-1 -- inner-sleep")
+                   (sleep-for 0 100))
+                 (when call-back (funcall call-back))))))))
 
 ;;; async-eval
 (defun python-ex:eval-async (code &optional call-back) (interactive "s\na")
@@ -268,31 +297,51 @@
                  (error (format fmt python-ex:eval-type))))))
 
 (defun python-ex:eval-internal-async (source-code &optional call-back)
-  (python-ex:with-async (source-code call-back)
-    (python-ex:let1 r (python-ex:eval-internal source-code)
-      (cond (call-back (funcall call-back r))
-            (t (message "pyex-result: %s" r))))))
+  (python-ex:with-async* 0.01 (source-code call-back)
+    (python-ex:eval-internal 
+     source-code t 
+     (or call-back (lambda (r) (message "pyex-result: %s" r))))))
 
 (defun python-ex:eval-external-async (source-code &optional call-back)
-  (python-ex:with-async (source-code call-back)
-    (python-ex:let1 r (python-ex:eval-external source-code)
-      (cond (call-back (funcall call-back r))
-            (t (message "pyex-result: %s" r))))))
+  (let ((tmpbuf " *python-ex:sentinel*")
+        (file (python-ex:gensym-filename)))
+    (with-current-buffer (get-buffer-create tmpbuf)
+      (erase-buffer))
+    (with-temp-file file
+      (insert source-code))
+    (python-ex:with-lexical-bindings (call-back tmpbuf)
+      (set-process-sentinel
+       (start-process-shell-command 
+        "python-ex:external" tmpbuf python-ex:python-command file)
+       (lambda (status &rest args)
+         (python-ex:let1 r (with-current-buffer tmpbuf (buffer-string))
+           (cond (call-back (funcall call-back r))
+                 (t (message "pyex-result: %s" r)))))))))
+
 ;;; repl-action
 (defvar python-ex:async-error-buffer " *python-ex async error*")
 
 (defmacro python-ex:with-async (args &rest actions)
-  (declare (indent 1))
+  `(python-ex:with-async* 0.1 ,args ,@actions))
+
+(defmacro python-ex:with-async* (secs args &rest actions)
+  (declare (indent 2))
   (python-ex:with-gensyms (err)
-  `(python-ex:with-lexical-bindings ,args
-     (run-with-idle-timer 
-      0.01 nil
-      (lambda ()
-        (condition-case ,err
-            (progn ,@actions)
-          (error (with-current-buffer (get-buffer-create python-ex:async-error-buffer)
-                   (insert (prin1-to-string ,err)))
-                 (display-buffer python-ex:async-error-buffer))))))))
+    `(python-ex:with-lexical-bindings ,args
+       (run-with-idle-timer
+        ,secs nil
+        (lambda ()
+          (condition-case ,err
+              (progn ,@actions)
+            (error (with-current-buffer (get-buffer-create python-ex:async-error-buffer)
+                     (insert (prin1-to-string ,err)))
+                   (display-buffer python-ex:async-error-buffer))))))))
+
+(defun python-ex:wait-for (dsecs finish-check &optional wait-call-back)
+  (cond ((funcall finish-check) (when wait-call-back (funcall wait-call-back)))
+        (t (python-ex:with-async* dsecs (dsecs finish-check wait-call-back)
+             (python-ex:debug-info "python-ex:wait-for -- in wait-for loop")
+             (python-ex:wait-for dsecs finish-check wait-call-back)))))
 
 (defmacro python-ex:with-action-repl-buffer (&rest actions)
   (python-ex:with-gensyms (w)
@@ -330,5 +379,62 @@
      (progn (beginning-of-defun) (point))
      (progn (end-of-defun) (point)))))
 
-(provide 'python-ex)
-  
+;;;; import
+(python-ex:with-anything
+ (defvar python-ex:all-modules-cache-buffer nil)
+
+ (defun python-ex:all-modules-cache-buffer (&optional force-reloadp asyncp showp)
+  (or (and python-ex:all-modules-cache-buffer
+           (not force-reloadp))
+      (python-ex:let1 code "
+import pydoc
+import sys
+from os.path import dirname
+
+sys.path.remove(dirname(sys.argv[0]))
+
+def all_modules(is_subpath_enable=True):
+    r = []
+    def callback(path, modname, desc):
+        if  is_subpath_enable or modname.find(\".\") < 0:
+            r.append(modname)
+    pydoc.ModuleScanner().run(callback)
+    return sorted(r)
+
+for i in  all_modules():
+    print i
+"
+        (python-ex:with-lexical-bindings (showp)
+          (lexical-let ((insert-result-to-buffer
+                         (lambda (r) 
+                           (python-ex:let1 buf (get-buffer-create " *python modules*")
+                             (with-current-buffer buf
+                               (erase-buffer)
+                               (insert r)
+                               (when showp (display-buffer " *python modules*"))
+                               (setq python-ex:all-modules-cache-buffer buf))))))
+            (cond (asyncp (python-ex:eval-external-async code insert-result-to-buffer))
+                  (t (funcall insert-result-to-buffer (python-ex:eval-external code))))))))))
+        
+;;          (help 
+;;           (lambda (c)
+;;             (python-ex:message-with-other-buffer
+;;              (lambda ()
+;;                (python-ex:eval
+;;                 (format "
+;; import %s
+;; help('%s')" 
+;;                         c c)))))))
+;;      (let ((source
+;;             `((name . "import")
+;;               (init . (lambda ()
+;;                         (with-current-buffer (anything-candidate-buffer 'global)
+;;                           (insert (python-ex:eval ,code)))))
+;;               (candidates-in-buffer)
+;;               (action . (("insert" . (lambda (c) (insert (format "import %s" c))))
+;;                          ("help" . ,help)))
+;;               (persistent-action . ,help))))
+;;        (anything-other-buffer (list source) " *python import*"))))
+
+ (provide 'python-ex)
+ 
--- python-ex.tmp.el
+++ python-ex.tmp.el
@@ -15,11 +15,30 @@
 (setq python-ex:auto-scroll-p t)
 (python-ex:send-string
  "for i in range(10):
-    print i,i,i
-" (lambda () (insert "foo")))
+    print i,i,i" (lambda () (insert "foo")))
 
-(python-ex:eval-internal-async
- "print 10"
+(python-ex:eval-external-async
+ "import time
+time.sleep(3)
+print 10"
  (lambda (r) (insert r)))
 ;;;
+(python-ex:all-modules-cache-buffer t t t)
+(setq python-ex:debug-info-p t)
+(setq xxxx t)
+(python-ex:wait-for 
+ 0.5 (lambda () (null xxxx))
+ (lambda () (mesage "hey!")))
+(setq xxxx nil)
+                       
+;; (while python-ex:eval-reading-p ;;polling
+;;   (python-ex:debug-info "inner-sleep")
+;;   (sleep-for 0 100))
+;;(pythno-ex:wait-for 100 python-ex:eval-reading-p)
 
+(defun python-ex:wait-for (secs pred)
+  (python-ex:let1 finishied-p (if (functionp pred) (fucall pred) pred)
+    (unless finishied-p
+      (run-with-timer
+       secs nil
+       (python-ex:cut 'python-ex:wait-for secs pred)))))
